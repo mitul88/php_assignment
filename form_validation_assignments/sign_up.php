05d879b6cd87f56d5b0907467e65d66ca8a09e07
shahed <shahed.rahman88@gmail.com>
2021-02-26 21:57:45 +0600

    add main element and padding to it

--- form_validation_assignments/sign_up.php
+++ form_validation_assignments/sign_up.php
@@ -19,8 +19,8 @@
         </style>
     </head>
     <body>
-
-    <div class="wrap shadow">
+    <div class="main py-5">
+        <div class="wrap shadow my-2">
         <div class="card">
             <div class="card-title text-center">
                 <h2 style="margin-top:20px;">Student Form</h2>
@@ -79,6 +79,8 @@
             </div>
         </div>
     </div>    
+    </div>
+    
     <!-- bootstrap scripts -->
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
