846eb34b633f8852048cdf5ad1cfc099324693c5
shahed <shahed.rahman88@gmail.com>
2021-02-27 16:31:56 +0600

    add image preview functionality to image_upload.php

--- form_validation_assignments/image_upload.php
+++ form_validation_assignments/image_upload.php
@@ -82,6 +82,9 @@
 
             ?>
                 <form action="" method="post" enctype="multipart/form-data">
+                   <div class="form-group">
+                       <img style="max-width:300px;" id="preview" src="" alt="">
+                   </div>
                     <div class="form-row">
                         <div class="form-group text-center col-sm-12">
                             <label for="img_upload"><img width="100" src="upload_img.png" alt=""></label>
@@ -101,5 +104,20 @@
         <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js" integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous"></script>
+        <script>
+            $(function () {
+                $('[data-toggle="tooltip"]').tooltip()
+            })
+
+            
+            $('input[name="upload_photo"]').change(function(e){
+
+                let file_url = URL.createObjectURL(e.target.files[0]);
+
+                $('img#preview').attr('src', file_url);
+
+            });
+        </script>
     </body>
     </html>
+    
